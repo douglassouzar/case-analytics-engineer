bcb0523351610aae6d912abf03481118957a6da7
Douglas Rodrigues <douglassouza.admtec@gmail.com>
2026-09-09 14:57:42 -0300

    feat: correções realizadas no stg e adicionado projeto de governance novo para validação e entendimento

--- dbt/rentcars_analytics/models/staging/stg_partners.sql
+++ dbt/rentcars_analytics/models/staging/stg_partners.sql
@@ -2,14 +2,14 @@ with base as (
     select * from {{ source('raw', 'raw_partners') }}
 )
 
-    select
-        partner_id,
-        partner_name,
-        upper(trim(country)) as country,
-        lower(trim(tier)) as tier,
-        lower(trim(status)) as status,
-        commission_rate,
-        cast(created_at as timestamp) as created_at,
-        cast(updated_at as timestamp) as updated_at,
-        lower(trim(contact_email)) as contact_email
-    from base
+select
+    partner_id,
+    partner_name,
+    upper(trim(country)) as country,
+    lower(trim(tier)) as tier,
+    lower(trim(status)) as status,
+    commission_rate,
+    cast(created_at as timestamp) as created_at,
+    cast(updated_at as timestamp) as updated_at,
+    lower(trim(contact_email)) as contact_email
+from base
